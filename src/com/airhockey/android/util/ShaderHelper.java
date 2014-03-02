82cd9b6fa9fcb94271c1d22ed162cfd80a45d5b3
Sean Jones <neuralsandwich@gmail.com>
2014-03-02 15:59:52 +0000

    ShaderHelper: Added compileShader

--- src/com/airhockey/android/util/ShaderHelper.java
+++ src/com/airhockey/android/util/ShaderHelper.java
@@ -1,8 +1,15 @@
 package com.airhockey.android.util;
 
-import static android.opengl.GLES20.*;
-import static android.opengl.GLUtils.*;
-import static android.opengl.Matrix.*;
+import static android.opengl.GLES20.GL_COMPILE_STATUS;
+import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES20.GL_VERTEX_SHADER;
+import static android.opengl.GLES20.glCompileShader;
+import static android.opengl.GLES20.glCreateShader;
+import static android.opengl.GLES20.glDeleteShader;
+import static android.opengl.GLES20.glGetShaderInfoLog;
+import static android.opengl.GLES20.glGetShaderiv;
+import static android.opengl.GLES20.glShaderSource;
+import android.util.Log;
 
 public class ShaderHelper {
 	private static final String TAG = "ShaderHelper";
@@ -19,7 +26,41 @@ public class ShaderHelper {
 	
 	public static int compileShader(int type, String shaderCode)
 	{
-		// TODO
-		return 0;
+		final int shaderObjectId = glCreateShader(type);
+		
+		if (shaderObjectId == 0) {
+			if (LoggerConfig.ON) {
+				Log.w(TAG, "Could not create new shader.");
+			}
+			return 0;
+		}
+		
+		// Pass the shader source.
+		glShaderSource(shaderObjectId, shaderCode);
+		
+		// Compile shader source.
+		glCompileShader(shaderObjectId);
+		
+		final int[] compileStatus = new int[1];
+		
+		glGetShaderiv(shaderObjectId, GL_COMPILE_STATUS, compileStatus, 0);
+		
+		if (LoggerConfig.ON){
+			// Print the shader info log to the Android log output.
+			Log.v(TAG, "Results of compiling source:" + "\n" + shaderCode
+					+ "\n:" + glGetShaderInfoLog(shaderObjectId));
+		}
+		
+		if (compileStatus[0] == 0) {
+			// if it filed, delete the shader object.
+			glDeleteShader(shaderObjectId);
+			
+			if (LoggerConfig.ON) {
+				Log.w(TAG, "Compilation of shader failed.");
+			}
+			return 0;
+		}
+		
+		return shaderObjectId;
 	}
 }
