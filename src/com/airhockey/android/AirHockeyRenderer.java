76fbca8d9d6d0e4c38ecb3f19758aec93de76f90
Sean Jones <neuralsandwich@gmail.com>
2014-03-02 13:50:33 +0000

    Updated renderer

--- src/com/airhockey/android/AirHockeyRenderer.java
+++ src/com/airhockey/android/AirHockeyRenderer.java
@@ -5,27 +5,64 @@ import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glViewport;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
 
 public class AirHockeyRenderer implements Renderer {
 
+	private static final int POSITION_COMPONENT_COUNT = 2;
+	private static final int BYTES_PER_FLOAT = 4;
+	private final FloatBuffer vertexData;
+	
+	public AirHockeyRenderer()
+	{
+		float[] tableVerticesWithTriangles = {
+			// Triangle 1
+			0f, 0f,
+			9f, 14f,
+			0f, 14f,
+			// Triangle 2
+			0f, 0f,
+			9f, 0f,
+			9f, 14f,
+			// Line 1
+			0f, 7f,
+			9f, 7f,
+			// Mallets
+			4.5f, 2f,
+			4.5f, 12f
+		};
+		
+		vertexData = ByteBuffer.allocateDirect(
+				tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+				.order(ByteOrder.nativeOrder()).asFloatBuffer();
+		vertexData.put(tableVerticesWithTriangles);
+	}
+	
 	@Override
-	public void onDrawFrame(GL10 glUnsued) {
+	public void onDrawFrame(GL10 glUnsued)
+	{
 		// Clear the rendering surface.
 		glClear(GL_COLOR_BUFFER_BIT);
 	}
 
 	@Override
-	public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+	public void onSurfaceChanged(GL10 glUnused, int width, int height)
+	{
 		// Set the OpenGL viewport to fill the entire surface.
 		glViewport(0, 0, width, height);
 	}
 
 	@Override
-	public void onSurfaceCreated(GL10 glUnsued, EGLConfig config) {
+	public void onSurfaceCreated(GL10 glUnsued, EGLConfig config)
+	{
 		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 	}
 
