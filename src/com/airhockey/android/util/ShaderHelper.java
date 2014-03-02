810822dc473c2252a4f9271386219097c1b9c188
Sean Jones <neuralsandwich@gmail.com>
2014-03-02 16:26:51 +0000

    ShaderHelper: Added LinkProgram

--- src/com/airhockey/android/util/ShaderHelper.java
+++ src/com/airhockey/android/util/ShaderHelper.java
@@ -2,12 +2,19 @@ package com.airhockey.android.util;
 
 import static android.opengl.GLES20.GL_COMPILE_STATUS;
 import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES20.GL_LINK_STATUS;
 import static android.opengl.GLES20.GL_VERTEX_SHADER;
+import static android.opengl.GLES20.glAttachShader;
 import static android.opengl.GLES20.glCompileShader;
+import static android.opengl.GLES20.glCreateProgram;
 import static android.opengl.GLES20.glCreateShader;
+import static android.opengl.GLES20.glDeleteProgram;
 import static android.opengl.GLES20.glDeleteShader;
+import static android.opengl.GLES20.glGetProgramInfoLog;
+import static android.opengl.GLES20.glGetProgramiv;
 import static android.opengl.GLES20.glGetShaderInfoLog;
 import static android.opengl.GLES20.glGetShaderiv;
+import static android.opengl.GLES20.glLinkProgram;
 import static android.opengl.GLES20.glShaderSource;
 import android.util.Log;
 
@@ -66,6 +73,41 @@ public class ShaderHelper {
 	
 	public static int linkProgram(int vertexShaderId, int fragmentShaderId)
 	{
-		return 0;
+		final int programObjectId = glCreateProgram();
+		
+		if (programObjectId == 0) {
+			if (LoggerConfig.ON) {
+				Log.w(TAG, "Could not create new program.");
+			}
+			return 0;
+		}
+		
+		// Attach shaders to program
+		glAttachShader(programObjectId, vertexShaderId);
+		glAttachShader(programObjectId, fragmentShaderId);
+		
+		// 
+		glLinkProgram(programObjectId);
+		
+		final int[] linkStatus = new int[1];
+		
+		glGetProgramiv(programObjectId, GL_LINK_STATUS, linkStatus, 0);
+		
+		if (LoggerConfig.ON) {
+			// Print the program info log to the Android log output
+			Log.v(TAG, "Results of linking program:\n"
+			+ glGetProgramInfoLog(programObjectId));
+		}
+		
+		if (linkStatus[0] == 0) {
+			// If it failed, delete the program object.
+			glDeleteProgram(programObjectId);
+			if (LoggerConfig.ON) {
+				Log.w(TAG, "Linking of progam failed.");
+			}
+			return 0;
+		}
+		
+		return programObjectId;
 	}
 }
