33d5a12e357007a7feac1d8f5485d75a17032139
Sean Jones <neuralsandwich@gmail.com>
2014-03-08 11:21:07 +0000

    AirHockeyRenderer: Increased table size

--- src/com/airhockey/android/AirHockeyRenderer.java
+++ src/com/airhockey/android/AirHockeyRenderer.java
@@ -59,19 +59,19 @@ public class AirHockeyRenderer implements Renderer {
 
 				// Triangle Fan
 				0f,    0f,   1f,   1f,   1f,
-				-0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-				0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-				0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-				-0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
-				-0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+				-0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+				 0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+				 0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+				-0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+				-0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
 
 				// Line 1
 				-0.5f, 0f, 1f, 0f, 0f,
-				0.5f, 0f, 1f, 0f, 0f,
+				 0.5f, 0f, 1f, 0f, 0f,
 
 				// Mallets
-				0f, -0.25f, 0f, 0f, 1f,
-				0f,  0.25f, 1f, 0f, 0f
+				0f, -0.4f, 0f, 0f, 1f,
+				0f,  0.4f, 1f, 0f, 0f
 		};
 
 		vertexData = ByteBuffer
